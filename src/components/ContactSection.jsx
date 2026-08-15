2cbb599aabdfe6a1f0f260b8d49099af516e96a8
Pasupuletivenkatababu <venkatababupasupuleti@gmail.com>
2026-08-15 23:52:35 +0530

    Update website

--- src/components/ContactSection.jsx
+++ src/components/ContactSection.jsx
@@ -160,15 +160,15 @@ function ContactSection() {
           <div className="service-hours">
             <div className="hour">
               <span className="day">Monday - Friday</span>
-              <span className="time">9:30 AM - 7:00 PM</span>
+              <span className="time">10:30 AM - 7:30 PM</span>
             </div>
             <div className="hour">
               <span className="day">Saturday</span>
-              <span className="time">10:30 AM - 7:00 PM</span>
+              <span className="time">11:00 AM - 7:00 PM</span>
             </div>
             <div className="hour">
               <span className="day">Sunday</span>
-              <span className="time">11:30 AM - 4:00 PM</span>
+              <span className="time">11:30 AM - 5:00 PM</span>
             </div>
           </div>
         </div>
